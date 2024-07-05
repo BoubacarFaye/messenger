a1cf9e8939be88a12edafb789e81227be75a0927
BoubacarFaye <146725708+BoubacarFaye@users.noreply.github.com>
2024-07-05 16:36:43 +0200

    new comment

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -1,18 +1,47 @@
 <?php
 
 namespace App\Controller;
-
+use App\Repository\CommentRepository;
+use App\Entity\Comment;
+use App\Form\CommentType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CommentController extends AbstractController
 {
-    #[Route('/comment', name: 'app_comment')]
-    public function index(): Response
+    public function __construct(private CommentRepository $CommentRepository)
     {
+
+    }
+    // TODO recup commentaires selon id, formulaire de nouveau commentaire
+    #[Route('/comment/post/{postId}', name: 'app_comment')]
+    public function index(int $postId): Response
+    {
+
+        //TODO verif si user connecte, si non, masquer inputs
+
+        $commentsList = $this->CommentRepository->fetchComments($postId);
+        $count = count($commentsList);
+        
         return $this->render('comment/index.html.twig', [
-            'controller_name' => 'CommentController',
+            'comments' => $commentsList,
+            'count' => $count,
+            'parentPost' => $postId,
+
         ]);
     }
+
+
+    #[Route('/comment/{postId}/new', name: 'app_new_comment')]
+    public function new(int $postId): Response
+    {
+        $comment = new Comment();
+        $form = $this->createForm(CommentType::class, $comment);
+
+        return $this->render('comment/new.html.twig', [
+            'comment_form' => $form
+        ]);
+    }
+
 }
