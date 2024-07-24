5346cda9875bbff30560ae1d3125a99ced32b998
BoubacarFaye <146725708+BoubacarFaye@users.noreply.github.com>
2024-07-24 14:27:09 +0200

    ADD: translation

--- src/Form/CommentType.php
+++ src/Form/CommentType.php
@@ -11,15 +11,22 @@ use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class CommentType extends AbstractType
 {
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
         ->add('content', TextareaType::class ,[
             'required'=> true,
-            'label'=> 'Message :',
+            'label'=> $this->translator->trans('comment'),
             'attr' =>['class'=>'flex flex-col gap-2 w-96']
         ])
         ->add('Envoyer', SubmitType::class, [
